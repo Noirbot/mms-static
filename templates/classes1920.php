db4098f76823e67f9f773e6033893b450232bf9e
Perry Shuman <thenoirbot@gmail.com>
2019-02-08 20:33:04 -0600

    19-20 updates.

---
+++ templates/classes1920.php
@@ -0,0 +1,72 @@
+<?php include('deps/m_header.php'); ?>
+            <h1> 2019-2020 Class Locations</h1>
+            <br />
+            <ul>
+              <li> <a target="_blank" href="http://www.eastsideacademicstudies.com"><span>Eastside Academic Studies</a> </li>
+              <li> <a target="_blank" href="http://www.metroacademicstudies.com/"><span>Metro Academic Studies (MAS)</a></li>
+            </ul>
+           </div>
+        </div>
+        <div class="clear"></div>
+      </div>
+      <div class="clear"></div>
+    </div>
+    <div id="main_middle">
+  <div id="middle_gradient_top"></div>
+
+  <div class="mid">
+
+<div style="padding:30px">
+  <h3><a name="classes">2019-20 Schedule and Locations</a> </h3>
+<table align="center" border="1" width="100%" class="schedule">
+  <tr>
+    <th width="10%"> </th>
+    <th width="30%" align="center"> <h2>Mon at <a target="_blank" href="http://www.metroacademicstudies.com/">MAS</a></h2> </th>
+    <th width="30%" align="center"> <h2>Tue at <a target="_blank" href="http://www.eastsideacademicstudies.com">Eastside</a></h2> </th>
+    <th width="30%" align="center"> <h2>Thu at <a target="_blank" href="http://www.eastsideacademicstudies.com">Eastside</a></h2> </th>
+  </tr>
+  <tr>
+    <td>9:00</td>
+    <td><a href="classes1819.html#LitComp">Multicultural Lit</a></td>
+    <td><a href="classes1819.html#BL">British Lit</a></td>
+    <td><a href="classes1819.html#LitComp">Multicultural Lit</a></td>
+  </tr>
+  <tr>
+    <td>10:45</td>
+    <td><a href="classes1819.html#APLit">AP Literature</a></td>
+    <td><a href="classes1819.html#BL">British Lit</a>, cont</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>12:15</td>
+    <td><em>Lunch</em></td>
+    <td><em>Lunch</em></td>
+    <td><em>Lunch</em></td>
+  </tr>
+  <tr>
+    <td>12:45</td>
+    <td><a href="classes1819.html#APLit">AP Literature</a>, cont until 2:00</td>
+    <td><a href="classes1819.html#APLit">AP Literature</a></td>
+    <td><a href="classes1819.html#LitComp">Multicultural Lit</a></td>
+  </tr>
+  <tr>
+    <td>2:30</td>
+    <td></td>
+    <td><a href="classes1819.html#BL">AP Literature</a>, cont until 3:45</td>
+    <td></td>
+  </tr>
+</table>
+
+  <br />
+  <a name="BL">  </a>    <?php include('deps/BritishLit.php');?>
+  <hr/>
+  <a name="LitComp">  </a>  <?php include('deps/LitComp.php');?>
+  <hr/>
+  <a name="APLit">  </a> <?php include('deps/APLit.php');?>
+
+</div>
+
+
+
+  <!-- <div class="clear"></div> -->
+<?php include('deps/footer.php');?>
